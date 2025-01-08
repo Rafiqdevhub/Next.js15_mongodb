4bd14dfe8664f856c3a8c545c9d320e49254d0a8
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-08 21:24:17 +0500

    rename application title in Navbar component from GTCoding to LearnByHeart

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
       <Link className="text-white font-bold" href={"/"}>
-        GTCoding.
+        LearnByHeart
       </Link>
       <Link className="bg-white p-2" href={"/addTopic"}>
         Add Topic
